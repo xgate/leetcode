b38d8b2d25be1f4573e7b64de78e3598b805dd57
chimun oh <cmohgate@gmail.com>
2022-05-12 23:39:49 +0900

    Time: 835 ms (5.04%), Space: 17.4 MB (57.64%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Two Pointers 
+# - meet again continuously on same node.
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        # find cycle
+        p1 = head
+        p2 = head
+        while True:
+            p1 = p1.next
+            p2 = p2.next
+            if not p1 or not p2:
+                return None
+            p2 = p2.next
+            if not p2:
+                return None
+            if p1 == p2:
+                break
+                
+        # has cycle. find cycle enterance.
+        p = head
+        while True:
+            while True:
+                if p == p1:
+                    return p
+                p1 = p1.next
+                if p1 == p2:
+                    break
+                
+            p = p.next
+            
+        return None
