002be13598649bd7f72f8780dfe28b72a62a0421
chimun oh <cmohgate@gmail.com>
2022-06-05 23:22:11 +0900

    Time: 100 ms (66.36%), Space: 13.9 MB (68.02%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        # s1 dict
+        s1d = dict()
+        for c in s1:
+            s1d[c] = s1d.get(c, 0)+1
+        # initial window
+        s2d = dict()
+        for i in range(len(s1)):
+            s2d[s2[i]] = s2d.get(s2[i], 0)+1
+        # move window
+        l, r = 0, i
+        while True:
+            if s1d == s2d:
+                return True
+            # remove left element
+            s2d[s2[l]] -= 1
+            if s2d[s2[l]] == 0:
+                del(s2d[s2[l]])
+            l += 1
+            r += 1
+            if r >= len(s2):
+                break
+            # insert right element
+            s2d[s2[r]] = s2d.get(s2[r], 0)+1
+        
+        return False
