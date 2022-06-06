ba34adc9e4673a7d8febbe5c209b5ded90d07a42
chimun oh <cmohgate@gmail.com>
2022-06-06 11:30:38 +0900

    Time: 209 ms (64.08%), Space: 14.5 MB (7.65%) - LeetHub

---
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.py
@@ -0,0 +1,22 @@
+class Solution:
+    
+    def r2dict(self, rs):
+        d = dict()
+        for i in range(len(rs)):
+            d[rs[i]] = i
+        return d
+    
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        r1 = self.r2dict(list1)
+        r2 = self.r2dict(list2)
+        commons = list(set(r1.keys()) & set(r2.keys()))
+        mergedR = {r: r1[r]+r2[r] for r in commons}
+        sortedR = sorted(mergedR.items(), key = lambda item: item[1])
+        least = sortedR[0][1]
+        ans = list()
+        for (r, i) in sortedR:
+            if i == least:
+                ans.append(r)
+            else:
+                break
+        return ans
