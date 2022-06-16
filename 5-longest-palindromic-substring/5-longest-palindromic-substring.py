9f65ad95a429f4b4ff07943baf6129a3bae1ebaa
chimun oh <cmohgate@gmail.com>
2022-06-16 21:53:07 +0900

    Time: 2050 ms (30.07%), Space: 13.8 MB (90.68%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,43 +1,21 @@
+# refactoring 
 class Solution:
     
-    def palindromeRange(self, s, l, r):
-        while s[l] == s[r]:
-            if l >= 1 and r < len(s)-1:
-                l -= 1
-                r += 1
-            else:
-                break
-        if s[l] != s[r]:
-            l += 1
-            r -= 1
-        return (l, r)
+    def palindromeLen(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return r-l-1
     
     def longestPalindrome(self, s: str) -> str:
-        d = dict()
-        maxLen = 0
+        start = end = 0
         for i in range(len(s)):
-            l1, r1 = self.palindromeRange(s, i, i)
-            length1 = r1-l1+1
+            len1 = self.palindromeLen(s, i, i)
             # consider 'bb', 'aaaa'
-            l2 = r2 = length2 = 0
-            if i < len(s)-1 and s[i] == s[i+1]:
-                l2, r2 = self.palindromeRange(s, i, i+1)
-                length2 = r2-l2+1            
-            if length1 >= length2:
-                l, r = l1, r1
-            else:
-                l, r = l2, r2            
-            
-            length = r-l+1
-            if length > 1:
-                d[length] = (l, r)
-            maxLen = max(length, maxLen)
+            len2 = self.palindromeLen(s, i, i+1)
+            length = max(len1, len2)
+            if length > end - start:
+                start = i-int((length-1)/2)
+                end = i+int(length/2)
         
-        if maxLen == 1:
-            return s[0]
-        
-        ans = ""
-        l, r = d[maxLen]
-        for i in range(l, r+1):
-            ans += s[i]    
-        return ans
+        return s[start:end+1]
