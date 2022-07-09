30a01042c97e6a718a7a03355e80538cd721cee6
chimun oh <cmohgate@gmail.com>
2022-07-09 13:00:39 +0900

    Time: 1566 ms (6.28%), Space: 29 MB (15.83%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,43 +1,32 @@
 from queue import PriorityQueue
 
 class Solution:
-    def canReach(self, heights, nth, bricks, ladders):
-        b, q = 0, PriorityQueue()
-        for i in range(1, nth+1):
-            diff = heights[i]-heights[i-1]
-            if diff > 0:
-                q.put(-diff)
-                b += diff
-        
-        if bricks >= b or ladders >= q.qsize():
-            return True
-        
-        for i in range(ladders):
-            q.get()
-        
-        while not q.empty():
-            bricks -= (-q.get())
-            if bricks < 0:
-                return False
-        
-        return True
-
-    
-    # O(logN*(2N+N*logN))
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        
         if len(heights) == 1:
             return 0
         
-        if self.canReach(heights, len(heights)-1, bricks, ladders):
-            return len(heights)-1
+        q = PriorityQueue(maxsize=ladders)
+        totalBricks = bricksInQue = 0
+        for i in range(1, len(heights)):
+            diff = heights[i]-heights[i-1]
+            if diff < 0:
+                continue
+            totalBricks += diff
+            if ladders > 0:
+                if q.full():
+                    minDiff = q.get()
+                    if diff > minDiff:
+                        q.put(diff)
+                        bricksInQue += (diff-minDiff)
+                    else:
+                        q.put(minDiff)
+                else:
+                    q.put(diff)
+                    bricksInQue += diff
         
-        l, r = 0, len(heights)-1
-        while l <= r:
-            m = int((l+r)/2)
-            if self.canReach(heights, m, bricks, ladders):
-                l = m+1
-            else:
-                r = m-1
+            if bricks < (totalBricks-bricksInQue):
+                return i-1
         
-        return r if m > r else m
+        return len(heights)-1
     
