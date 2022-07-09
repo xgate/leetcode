9d779032e0ebccf68664d054489d4d798258d47f
chimun oh <cmohgate@gmail.com>
2022-07-09 22:40:14 +0900

    Time: 1385 ms (9.33%), Space: 28.8 MB (15.83%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,31 +1,24 @@
 from queue import PriorityQueue
 
 class Solution:
-    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:        
         
         if len(heights) == 1:
             return 0
         
-        q = PriorityQueue(maxsize=ladders)
-        totalBricks = bricksInQue = 0
+        b, q = 0, PriorityQueue()
+        
         for i in range(1, len(heights)):
             diff = heights[i]-heights[i-1]
-            if diff < 0:
-                continue
-            totalBricks += diff
-            if ladders > 0:
-                if q.full():
-                    minDiff = q.get()
-                    if diff > minDiff:
-                        q.put(diff)
-                        bricksInQue += (diff-minDiff)
-                    else:
-                        q.put(minDiff)
-                else:
+            if diff > 0:
+                if ladders > 0:
                     q.put(diff)
-                    bricksInQue += diff
-        
-            if bricks < (totalBricks-bricksInQue):
+                    if q.qsize() > ladders:
+                        b += q.get()
+                else:
+                    b += diff
+                        
+            if b > bricks:
                 return i-1
         
         return len(heights)-1
